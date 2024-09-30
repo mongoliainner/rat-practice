447b0e5f3e09ecc30980d9df372551e77871bc1e
= <yuexrender@gmail.com>
2024-09-30 04:19:50 +0300

    fixed: canvas

--- src/components/0_wrappers/Canvas.js
+++ src/components/0_wrappers/Canvas.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 export function Canvas({ children, padding = "1rem", gap = "0.5rem" }) {
+  const canvasRef = useRef(null);
+
+  const drawOnCanvas = () => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    canvas.width = canvas.parentElement.clientWidth;
+    canvas.height = canvas.parentElement.clientHeight;
+
+    context.fillStyle = "blue";
+    context.fillRect(50, 50, 150, 100);
+
+    context.beginPath();
+    context.arc(300, 300, 50, 0, Math.PI * 2);
+    context.fillStyle = "green";
+    context.fill();
+  };
+
+  useEffect(() => {
+    drawOnCanvas();
+  }, []);
+
   const containerStyle = {
     display: "flex",
     flexDirection: "column",
     padding: padding,
     gap: gap,
-    height: "100",
+    height: "100vh",
   };
 
-  return <canvas></canvas>;
+  return (
+    <div style={containerStyle}>
+      <canvas ref={canvasRef}></canvas>
+      {children}
+    </div>
+  );
 }
